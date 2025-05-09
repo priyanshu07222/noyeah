070d21dc82b86b60be8bdb1f600258402a832840
Priyanshu Tiwari <priyanshutiwari@Priyanshus-MacBook-Air.local>
2025-05-09 15:53:00 +0530

    made module

--- app/api/contest/list/route.ts
+++ app/api/contest/list/route.ts
@@ -1,9 +1,13 @@
 
 
-async function GET(req: Request){
+export async function GET(req: Request){
     const title = await req.body;
     
     if(!title){
         return null;
     }
+
+    Response.json({
+        message: "done get the data"
+    })
 }
